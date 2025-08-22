905c7be0f1337ab8bb3b8939ec2c2deb81b943b1
Hemang <150885858+DevHemang@users.noreply.github.com>
2025-08-22 18:54:07 +0530

    schema change

--- school_management.sql
+++ school_management.sql
@@ -2,9 +2,11 @@ CREATE DATABASE school_management;
 USE school_management;
 
 CREATE TABLE schools (
-	id INT PRIMARY KEY,
+	id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     address VARCHAR(255) NOT NULL,
     latitude FLOAT NOT NULL,
     longitude FLOAT NOT NULL
 )
+SELECT * FROM schools;
+
