f9fbd5f59c38f87819ce2d4d82dc9271320b67e8
Hemang <150885858+DevHemang@users.noreply.github.com>
2025-08-22 23:33:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,21 @@ app.use(express.json());
 
 
 // MySQL connection setup
+/*const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});*/
+
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,  // Add this line!
 });
+
 
 // DB connection
 db.connect((err) => {
